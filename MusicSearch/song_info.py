c5b2fe6869b58462cba7febbd169c62f3cbf6baf
ethan-garnica <ethangarnica2@gmail.com>
2021-12-06 18:11:33 -0800

    added song info dictionary

---
+++ MusicSearch/song_info.py
@@ -0,0 +1,19 @@
+song_info = [
+    {
+        "artist" : "Michael Jackson",
+        "title" : "Thriller",
+        "album_cover" : "https://www.covercentury.com/covers/audio/m/Michael%20Jackson%20-%20Thriller;%2025th%20Anniversary%20Edition%20[vk%20-%20us].JPG",
+        "genre" : "Pop",
+        "year" : "1982"
+        "song_list" =  "https://open.spotify.com/embed/album/2ANVost0y2y52ema1E9xAZ?utm_source=generator"
+    },
+    {
+        "artist" : "AC/DC",
+        "title" : "Back in Black",
+        "album_cover" : "https://upload.wikimedia.org/wikipedia/commons/9/92/ACDC_Back_in_Black.png",
+        "genre" : "Rock",
+        "year" : "1982"
+        "song_list" =  "https://open.spotify.com/embed/album/6mUdeDZCsExyJLMdAfDuwh?utm_source=generator"
+    }
+
+] 
