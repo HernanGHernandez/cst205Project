faee0b0410c9d7c2b8579c66cc4f40a1fce216c7
ethan-garnica <ethangarnica2@gmail.com>
2021-12-08 17:53:45 -0800

    fixed song list

--- MusicSearch/song_info.py
+++ MusicSearch/song_info.py
@@ -18,7 +18,7 @@ song_info = [
     {
         "artist" : "Meat Loaf",
         "title" : "Bat Out of Hell",
-        "album_cover" : "https://static01.nyt.com/images/2021/04/03/multimedia/03xp-april/merlin_185893383_8e41433f-4a32-4b1e-bf02-457290d0d534-superJumbo.jpg",
+        "album_cover" : "https://m.media-amazon.com/images/I/71+JN133qiL._SL1200_.jpg",
         "genre" : "Rock",
         "year" : "1977",
         "song_list" :  "https://open.spotify.com/embed/album/6mvI80w5r78niBmwtu7RF9?utm_source=generator"
@@ -26,84 +26,85 @@ song_info = [
     {
         "artist" : "Pink Floyd",
         "title" : "The Dark Side of the Moon",
-        "album_cover" : "https://static01.nyt.com/images/2021/04/03/multimedia/03xp-april/merlin_185893383_8e41433f-4a32-4b1e-bf02-457290d0d534-superJumbo.jpg",
+        "album_cover" : "https://www.thisdayinmusic.com/wp-content/uploads/2018/05/the-dark-side-of-the-moon.jpg",
         "genre" : "Rock",
         "year" : "1973",
-        "song_list" :  "https://open.spotify.com/embed/album/6mvI80w5r78niBmwtu7RF9?utm_source=generator"
+        "song_list" :  "https://open.spotify.com/embed/album/4LH4d3cOWNNsVw41Gqt2kv?utm_source=generator"
     },
     {
         "artist" : "Eagles",
         "title" : "Hotel California",
-        "album_cover" : "https://static01.nyt.com/images/2021/04/03/multimedia/03xp-april/merlin_185893383_8e41433f-4a32-4b1e-bf02-457290d0d534-superJumbo.jpg",
+        "album_cover" : "https://m.media-amazon.com/images/I/71s+xlkhz8L._SL1425_.jpg",
         "genre" : "Rock",
         "year" : "1976",
-        "song_list" :  "https://open.spotify.com/embed/album/6mvI80w5r78niBmwtu7RF9?utm_source=generator"
+        "song_list" :  "https://open.spotify.com/embed/album/2widuo17g5CEC66IbzveRu?utm_source=generator"
     },
     {
         "artist" : "Fleetwood Mac",
         "title" : "Rumours",
-        "album_cover" : "https://static01.nyt.com/images/2021/04/03/multimedia/03xp-april/merlin_185893383_8e41433f-4a32-4b1e-bf02-457290d0d534-superJumbo.jpg",
+        "album_cover" : "https://m.media-amazon.com/images/I/71BekDJBb3L._SL1425_.jpg",
         "genre" : "Rock",
         "year" : "1977",
-        "song_list" :  "https://open.spotify.com/embed/album/6mvI80w5r78niBmwtu7RF9?utm_source=generator"
+        "song_list" :  "https://open.spotify.com/embed/album/0BwWUstDMUbgq2NYONRqlu?utm_source=generator"
     },
     {
         "artist" : "Shania Twain",
         "title" : "Come on Over",
-        "album_cover" : "https://static01.nyt.com/images/2021/04/03/multimedia/03xp-april/merlin_185893383_8e41433f-4a32-4b1e-bf02-457290d0d534-superJumbo.jpg",
+        "album_cover" : "https://cdn.sanity.io/images/o6uq28nb/production/1fd2f8410ba5bd406917ed8017f25d2eb109487b-1408x1404.jpg",
         "genre" : "Country",
         "year" : "1997",
-        "song_list" :  "https://open.spotify.com/embed/album/6mvI80w5r78niBmwtu7RF9?utm_source=generator"
+        "song_list" :  "https://open.spotify.com/embed/album/4UMe0Ods7kygK6OISasZe9?utm_source=generator"
     },
     {
         "artist" : "Led Zepplin",
         "title" : "Led Zepplin IV",
-        "album_cover" : "https://static01.nyt.com/images/2021/04/03/multimedia/03xp-april/merlin_185893383_8e41433f-4a32-4b1e-bf02-457290d0d534-superJumbo.jpg",
+        "album_cover" : "https://townsquare.media/site/295/files/2013/11/Led-Zeppelin-IV.jpg?w=1200&h=0&zc=1&s=0&a=t&q=89",
         "genre" : "Rock",
         "year" : "1971",
-        "song_list" :  "https://open.spotify.com/embed/album/6mvI80w5r78niBmwtu7RF9?utm_source=generator"
+        "song_list" :  "https://open.spotify.com/embed/album/5EyIDBAqhnlkAHqvPRwdbX?utm_source=generator"
     },
     {
         "artist" : "Michael Jackson",
         "title" : "Bad",
-        "album_cover" : "https://static01.nyt.com/images/2021/04/03/multimedia/03xp-april/merlin_185893383_8e41433f-4a32-4b1e-bf02-457290d0d534-superJumbo.jpg",
+        "album_cover" : "https://m.media-amazon.com/images/I/6148jblbNqL._SL1500_.jpg",
         "genre" : "Rop",
         "year" : "1987",
-        "song_list" :  "https://open.spotify.com/embed/album/6mvI80w5r78niBmwtu7RF9?utm_source=generator"
+        "song_list" :  "https://open.spotify.com/embed/album/3Us57CjssWnHjTUIXBuIeH?utm_source=generator"
     },
     {
         "artist" : "Alanis Morissette",
         "title" : "Jagged Little Pill",
-        "album_cover" : "https://static01.nyt.com/images/2021/04/03/multimedia/03xp-april/merlin_185893383_8e41433f-4a32-4b1e-bf02-457290d0d534-superJumbo.jpg",
+        "album_cover" : "https://m.media-amazon.com/images/I/81UsU658UVL._SL1425_.jpg",
         "genre" : "Rock",
         "year" : "1995",
-        "song_list" :  "https://open.spotify.com/embed/album/6mvI80w5r78niBmwtu7RF9?utm_source=generator"
+        "song_list" :  "https://open.spotify.com/embed/album/09AwlP99cHfKVNKv4FC8VW?utm_source=generator"
     },
     {
         "artist" : "Michael Jackson",
         "title" : "Dangerous",
-        "album_cover" : "https://static01.nyt.com/images/2021/04/03/multimedia/03xp-april/merlin_185893383_8e41433f-4a32-4b1e-bf02-457290d0d534-superJumbo.jpg",
+        "album_cover" : "https://m.media-amazon.com/images/I/91kghsxZPBL._SL1500_.jpg",
         "genre" : "Pop",
         "year" : "1991",
-        "song_list" :  "https://open.spotify.com/embed/album/6mvI80w5r78niBmwtu7RF9?utm_source=generator"
+        "song_list" :  "https://open.spotify.com/embed/album/0oX4SealMgNXrvRDhqqOKg?utm_source=generator"
     },
     {
         "artist" : "Celine Dion",
         "title" : "Falling into You",
-        "album_cover" : "https://static01.nyt.com/images/2021/04/03/multimedia/03xp-april/merlin_185893383_8e41433f-4a32-4b1e-bf02-457290d0d534-superJumbo.jpg",
+        "album_cover" : "https://m.media-amazon.com/images/I/7115ubghVQL._SL1500_.jpg",
         "genre" : "Pop",
         "year" : "1996",
-        "song_list" :  "https://open.spotify.com/embed/album/6mvI80w5r78niBmwtu7RF9?utm_source=generator"
+        "song_list" :  "https://open.spotify.com/embed/album/2vk0n8aeQSO74VtQBbNDPR?utm_source=generator"
     },
     {
         "artist" : "The Beatles",
         "title" : "Sgt. Pepper's Lonely Hearts Club Band",
-        "album_cover" : "https://static01.nyt.com/images/2021/04/03/multimedia/03xp-april/merlin_185893383_8e41433f-4a32-4b1e-bf02-457290d0d534-superJumbo.jpg",
+        "album_cover" : "https://townsquare.media/site/443/files/2015/06/emi.jpg?w=1200&h=0&zc=1&s=0&a=t&q=89",
         "genre" : "Rock",
         "year" : "1967",
-        "song_list" :  "https://open.spotify.com/embed/album/6mvI80w5r78niBmwtu7RF9?utm_source=generator"
+        "song_list" :  "https://open.spotify.com/embed/album/6QaVfG1pHYl1z15ZxkvVDW?utm_source=generator"
     },
     {
+        # need to finish working on links after this and add a few more albums
         "artist" : "Adele",
         "title" : "21",
         "album_cover" : "https://static01.nyt.com/images/2021/04/03/multimedia/03xp-april/merlin_185893383_8e41433f-4a32-4b1e-bf02-457290d0d534-superJumbo.jpg",
