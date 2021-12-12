789939a3b6b24e79cbf2144034e6201fab687f5c
Ethan Garnica <92120279+ethan-garnica@users.noreply.github.com>
2021-12-11 16:20:34 -0800

    updated and finished song_info.py
    There are 35 albums in this dictionary all having more at least 25 million sales

--- MusicSearch/song_info.py
+++ MusicSearch/song_info.py
@@ -104,47 +104,179 @@ song_info = [
         "song_list" :  "https://open.spotify.com/embed/album/6QaVfG1pHYl1z15ZxkvVDW?utm_source=generator"
     },
     {
-        # need to finish working on links after this and add a few more albums
         "artist" : "Adele",
         "title" : "21",
-        "album_cover" : "https://static01.nyt.com/images/2021/04/03/multimedia/03xp-april/merlin_185893383_8e41433f-4a32-4b1e-bf02-457290d0d534-superJumbo.jpg",
+        "album_cover" : "https://m.media-amazon.com/images/I/71Ln3JLWyOL._SL1500_.jpg",
         "genre" : "Pop",
         "year" : "2011",
-        "song_list" : "https://open.spotify.com/embed/album/6mvI80w5r78niBmwtu7RF9?utm_source=generator"
+        "song_list" : "https://open.spotify.com/embed/album/0Lg1uZvI312TPqxNWShFXL?utm_source=generator"
     },
     {
         "artist" : "Celine Dion",
         "title" : "Let's Talk About Love",
-        "album_cover" : "https://static01.nyt.com/images/2021/04/03/multimedia/03xp-april/merlin_185893383_8e41433f-4a32-4b1e-bf02-457290d0d534-superJumbo.jpg",
+        "album_cover" : "https://m.media-amazon.com/images/I/71bBg5xM5YL._SL1500_.jpg",
         "genre" : "Pop",
         "year" : "1997",
-        "song_list" :  "https://open.spotify.com/embed/album/6mvI80w5r78niBmwtu7RF9?utm_source=generator"
+        "song_list" :  "https://open.spotify.com/embed/album/1tfS7Fo1UtAxQSf256fnYs?utm_source=generator"
     },
     {
         "artist" : "Metallica",
         "title" : "Metallica",
-        "album_cover" : "https://static01.nyt.com/images/2021/04/03/multimedia/03xp-april/merlin_185893383_8e41433f-4a32-4b1e-bf02-457290d0d534-superJumbo.jpg",
+        "album_cover" : "https://m.media-amazon.com/images/I/71z4zm5yohL._SL1425_.jpg",
         "genre" : "Metal",
         "year" : "1991",
-        "song_list" :  "https://open.spotify.com/embed/album/6mvI80w5r78niBmwtu7RF9?utm_source=generator"
+        "song_list" :  "https://open.spotify.com/embed/album/55fq75UfkYbGMq4CncCtOH?utm_source=generator"
     },
     {
         "artist" : "The Beatles",
         "title" : "Abbey Road",
-        "album_cover" : "https://static01.nyt.com/images/2021/04/03/multimedia/03xp-april/merlin_185893383_8e41433f-4a32-4b1e-bf02-457290d0d534-superJumbo.jpg",
+        "album_cover" : "https://images.radiox.co.uk/images/67172?width=1400&crop=16_9&signature=oOzOBNznUl35DsUl5UoB-DVZlRQ=",
         "genre" : "Rock",
         "year" : "1969",
-        "song_list" :  "https://open.spotify.com/embed/album/6mvI80w5r78niBmwtu7RF9?utm_source=generator"
+        "song_list" :  "https://open.spotify.com/embed/album/0ETFjACtuP2ADo6LFhL6HN?utm_source=generator"
     },
     {
         "artist" : "Bruce Springsteen",
         "title" : "Born in the U.S.A.",
-        "album_cover" : "https://static01.nyt.com/images/2021/04/03/multimedia/03xp-april/merlin_185893383_8e41433f-4a32-4b1e-bf02-457290d0d534-superJumbo.jpg",
+        "album_cover" : "https://m.media-amazon.com/images/I/819qdTNLMxL._SL1500_.jpg",
         "genre" : "Rock",
         "year" : "1984",
-        "song_list" :  "https://open.spotify.com/embed/album/6mvI80w5r78niBmwtu7RF9?utm_source=generator"
+        "song_list" :  "https://open.spotify.com/embed/album/0PMasrHdpaoIRuHuhHp72O?utm_source=generator"
+    },
+    {
+        "artist" : "Pink Floyd",
+        "title" : "The Wall",
+        "album_cover" : "https://static.wikia.nocookie.net/pinkfloyd/images/f/f5/The_Wall.jpg/revision/latest?cb=20181104235825",
+        "genre" : "Rock",
+        "year" : "1979",
+        "song_list" :  "https://open.spotify.com/embed/album/5Dbax7G8SWrP9xyzkOvy2F?utm_source=generator"
+    },
+    {
+        "artist" : "Dire Straits",
+        "title" : "Brother in Arms",
+        "album_cover" : "https://m.media-amazon.com/images/I/71cTpDRudLL._SL1500_.jpg",
+        "genre" : "Rock",
+        "year" : "1985",
+        "song_list" :  "https://open.spotify.com/embed/album/7jvcSnCnugLcisBCNBm60s?utm_source=generator"
+    },
+    {
+        "artist" : "Santana",
+        "title" : "Supernatural",
+        "album_cover" : "https://m.media-amazon.com/images/I/91z6SsRHMdL._SL1500_.jpg",
+        "genre" : "Rock",
+        "year" : "1999",
+        "song_list" :  "https://open.spotify.com/embed/album/10aiDpdFGyfCFEcqpx6XTq?utm_source=generator"
+    },
+    {
+        "artist" : "Guns N' Roses",
+        "title" : "Appetite for Destruction",
+        "album_cover" : "https://m.media-amazon.com/images/I/71H9ZR6EGFL._SL1400_.jpg",
+        "genre" : "Rock",
+        "year" : "1987",
+        "song_list" :  "https://open.spotify.com/embed/album/28yHV3Gdg30AiB8h8em1eW?utm_source=generator"
+    },
+    {
+        "artist" : "Elton John",
+        "title" : "Goodbye Yellow Brick Road",
+        "album_cover" : "https://m.media-amazon.com/images/I/81Ls3DlV5YL._SL1081_.jpg",
+        "genre" : "Rock",
+        "year" : "1973",
+        "song_list" :  "https://open.spotify.com/embed/album/5WupqgR68HfuHt3BMJtgun?utm_source=generator"
+    },
+    {
+        "artist" : "Bon Jovi",
+        "title" : "Slippery When Wet",
+        "album_cover" : "https://m.media-amazon.com/images/I/919jd0Vs84L._SL1400_.jpg",
+        "genre" : "Rock",
+        "year" : "1986",
+        "song_list" :  "https://open.spotify.com/embed/album/0kBfgEilUFCMIQY5IOjG4t?utm_source=generator"
+    },
+    {
+        "artist" : "Mariah Carey",
+        "title" : "Music Box",
+        "album_cover" : "https://m.media-amazon.com/images/I/71BqAK-vXvL._SL1500_.jpg",
+        "genre" : "Pop",
+        "year" : "1993",
+        "song_list" :  "https://open.spotify.com/embed/album/2NKxb7pk04CuZab5udkGUl?utm_source=generator"
+    },
+    {
+        "artist" : "Linkin Park",
+        "title" : "Hybrid Theory",
+        "album_cover" : "https://vinylcountdownsite.files.wordpress.com/2017/07/img_4841.jpg",
+        "genre" : "Rock",
+        "year" : "2000",
+        "song_list" :  "https://open.spotify.com/embed/album/2pKw6GERJVAD61449B1EEM?utm_source=generator"
+    },
+    {
+        "artist" : "Eminem",
+        "title" : "The Eminem Show",
+        "album_cover" : "https://www.grammy.com/sites/com/files/styles/news_detail_header/public/the_eminem_show_1240x600.jpg?itok=Pa0v6zAA",
+        "genre" : "Hip Hop",
+        "year" : "2002",
+        "song_list" :  "https://open.spotify.com/embed/album/2cWBwpqMsDJC1ZUwz813lo?utm_source=generator"
+    },
+    {
+        "artist" : "Norah Jones",
+        "title" : "Come Away with Me",
+        "album_cover" : "https://m.media-amazon.com/images/I/61wzWyKjadL._SL1146_.jpg",
+        "genre" : "Jazz",
+        "year" : "2002",
+        "song_list" :  "https://open.spotify.com/embed/album/1JvoMzqg04nC29gam4Qaiq?utm_source=generator"
+    },
+    {
+        "artist" : "Britney Spears",
+        "title" : "...Baby One More Time",
+        "album_cover" : "https://m.media-amazon.com/images/I/71Thj-faSdL._SL1500_.jpg",
+        "genre" : "Pop",
+        "year" : "1999",
+        "song_list" :  "https://open.spotify.com/embed/album/3WNxdumkSMGMJRhEgK80qx?utm_source=generator"
+    },
+    {
+        "artist" : "Carole King",
+        "title" : "Tapestry",
+        "album_cover" : "https://images.squarespace-cdn.com/content/v1/565c1ab5e4b05079e4bfa169/1574667488675-5IWT2PK9KSE1J3YT3FKQ/Carole_King_Tapestry_Vinyl_Cover.jpg",
+        "genre" : "Pop",
+        "year" : "1971",
+        "song_list" :  "https://open.spotify.com/embed/album/12n11cgnpjXKLeqrnIERoS?utm_source=generator"
+    },
+    {
+        "artist" : "Phil Collins",
+        "title" : "No Jacket Required",
+        "album_cover" : "https://m.media-amazon.com/images/I/71cAR9b6XsL._SL1425_.jpg",
+        "genre" : "Rock",
+        "year" : "1985",
+        "song_list" :  "https://open.spotify.com/embed/album/1rVhockt4RAiZFaK3M3zPB?utm_source=generator"
+    },
+    {
+        "artist" : "Madonna",
+        "title" : "True Blue",
+        "album_cover" : "https://m.media-amazon.com/images/I/7194R8FlmrL._SL1089_.jpg",
+        "genre" : "Pop",
+        "year" : "1986",
+        "song_list" :  "https://open.spotify.com/embed/album/6fmnT17jc2Sc69q3nza1eD?utm_source=generator"
+    },
+    {
+        "artist" : "Simon & Garfunkel",
+        "title" : "Bridge Over Troubled Water",
+        "album_cover" : "https://m.media-amazon.com/images/I/81WYpgS+3JL._SL1500_.jpg",
+        "genre" : "Folk",
+        "year" : "1970",
+        "song_list" :  "https://open.spotify.com/embed/album/0JwHz5SSvpYWuuCNbtYZoV?utm_source=generator"
+    },
+    {
+        "artist" : "U2",
+        "title" : "The Joshua Tree",
+        "album_cover" : "https://m.media-amazon.com/images/I/71nhNKvy+fL._SL1059_.jpg",
+        "genre" : "Rock",
+        "year" : "1987",
+        "song_list" :  "https://open.spotify.com/embed/album/5vBZRYu2GLA65nfxBvG1a7?utm_source=generator"
+    },
+    {
+        "artist" : "George Michael",
+        "title" : "Faith",
+        "album_cover" : "https://www.classicpopmag.com/wp-content/uploads/2021/04/george-michael-faith.jpg",
+        "genre" : "Pop",
+        "year" : "1987",
+        "song_list" :  "https://open.spotify.com/embed/album/5E68z4OgQrR446xVOxZKs6?utm_source=generator"
     }
-
-
 ] 
-
